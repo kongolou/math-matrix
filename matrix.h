bac9e07d663dc93c87f0e4d22751cda9853390be
kongolou <126950255+kongolou@users.noreply.github.com>
2023-08-18 21:04:05 +0800

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -2,44 +2,43 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-#include <iostream>
-#include <iomanip>
 #include <cmath>
+#include <iomanip>
+#include <iostream>
 
-
-class Matrix
-{
+class Matrix {
 private:
-    int row;
-    int column;
-    double** pointer;
-protected:
-    void Initialize();
-    void Create();
-    void Destroy();
-public:
-    Matrix();
-    explicit Matrix(const int& dimension);
-    Matrix(const int& row, const int& column);
-    Matrix(const Matrix& other);
-    Matrix(Matrix&& other);
-    ~Matrix();
-    Matrix& operator=(const Matrix& other);
-    Matrix& operator=(Matrix&& other);
-    Matrix operator+(const Matrix& other);
-    Matrix operator-(const Matrix& other);
-    Matrix operator*(const Matrix& other);
-    friend Matrix operator*(const double& number, const Matrix& matrix);
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
-    void MakeZero();
-    void MakeIdentity();
-    void MakeFromSequence(const double* sequence);
-    double Determinant();
-    double Cofactor(const int& x, const int& y);
-    Matrix Transpose();
-    Matrix AdjointMatrix();
-    Matrix Inverse();
-};
+  int row;
+  int column;
+  double **pointer;
 
+protected:
+  void Initialize();
+  void Create();
+  void Destroy();
+
+public:
+  Matrix();
+  Matrix(const int &dimension);
+  Matrix(const int &row, const int &column);
+  Matrix(const Matrix &other);
+  Matrix(Matrix &&other);
+  ~Matrix();
+  Matrix &operator=(const Matrix &other);
+  Matrix &operator=(Matrix &&other);
+  Matrix operator+(const Matrix &other);
+  Matrix operator-(const Matrix &other);
+  Matrix operator*(const Matrix &other);
+  friend Matrix operator*(const double &number, const Matrix &matrix);
+  friend std::ostream &operator<<(std::ostream &os, const Matrix &matrix);
+  void MakeItZero();
+  void MakeItIdentity();
+  void MakeItFromSequence(const double *sequence);
+  double Determinant();
+  Matrix Transpose();
+  Matrix Submatrix(const int &x, const int &y);
+  Matrix AdjointMatrix();
+  Matrix Inverse();
+};
 
 #endif // MATRIX_H
